4d330f2e80ffc5c27b315960ab3be3f8655ca879
abdelhak elahrach <elahracheabdelhak@gmail.com>
2025-05-21 02:01:50 +0100

    basic auth system

--- app/(site)/tags/[title]/page.js
+++ app/(site)/tags/[title]/page.js
@@ -10,13 +10,13 @@ export async function generateMetadata({ params }) {
     if (!data) {
       return {
         title: `${title} | Tag not found`,
-        description: `No description available for tag "${title}".`,
+        description: `No description available for Tag "${title}".`,
       };
     }
 
     return {
-      title: `${data.title || title} | Articles in tag ${data.title || title}`,
-      description: data.description || `Articles tagged with "${title}".`,
+      title: `${data.title || title} | Articles in Tag ${data.title || title}`,
+      description: data.description || `Articles in Tag : "${title}".`,
       openGraph: {
         title: data.title,
         description: data.description,
@@ -30,7 +30,7 @@ export async function generateMetadata({ params }) {
   }
 }
 
-export default async function singleTag({ params }) {
+export default async function SingleCategory({ params }) {
   const ArticlePerPage = 8;
   const { title } = await params;
 
@@ -38,7 +38,7 @@ export default async function singleTag({ params }) {
 
   if(!data) {
     return (
-      <div className='error-message'>Tag not found</div>
+      <div className='error-message'>Category not found</div>
     )
   }
 
