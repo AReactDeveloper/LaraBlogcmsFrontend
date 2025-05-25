929ec02de0564192c1f75de771b87b31e886e615
abdelhak elahrach <elahracheabdelhak@gmail.com>
2025-05-25 07:04:49 +0100

    dd

--- app/admin/articles/add/page.js
+++ app/admin/articles/add/page.js
@@ -1,17 +1,7 @@
-'use client'
+'use client';
 
-import Head from 'next/head'
-import AddForm from './AddForm'
+import AddForm from './AddForm';
 
 export default function Page() {
-  return (
-    <>
-      <Head>
-        <title>Add new Article</title>
-        <meta name="description" content="" />
-      </Head>
-
-      <AddForm />
-    </>
-  )
+  return <AddForm />;
 }
